137c2af812fd05ab9350ecf60053e22a7ff70ee3
wonkatin <j.drawers@gmail.com>
2021-02-08 16:05:41 -0600

    Fifth Commit: rough draft of game design

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Constants */
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+console.log(colors)
+/* Game Logic Variables and State */
+let solution = []
+/* DOM References */
+
+/* Functions and Game Logic */
+randomizeColors = () => {
+
+}
+/* Event Listeners */
+
+// set solution
+// create reveal solution function
