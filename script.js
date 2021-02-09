ae786cdd267370521e90c68b2c22623dcf433e12
wonkatin <j.drawers@gmail.com>
2021-02-09 14:24:18 -0600

    randomizeColors is working, event listener updated

--- script.js
+++ script.js
@@ -8,26 +8,21 @@ let reveal = document.querySelector(".solution");
 let solutionOne = document.querySelector("#solution-one");
 let solutionTwo = document.querySelector("#solution-two");
 let solutionThree = document.querySelector("#solution-three");
-let options = document.querySelector(".options")
-// let red = document.querySelector("#red");
-// let orange = document.querySelector("#orange");
-// let yellow = document.querySelector("#yellow");
-// let green = document.querySelector("#green");
-// let blue = document.querySelector("#blue");
-// let purple = document.querySelector("#purple");
+let options = document.querySelectorAll(".option")
+console.log(options)
 
 /* Functions and Game Logic */
 randomizeColors = () => {
     for(i = 0; i < 3; i++) {
         let color = colors[Math.floor(Math.random() * colors.length)];
-        if (!solution.includes(color)){
-                    solution[i] = color;
+        while(solution.includes(color)){
+            color = colors[Math.floor(Math.random() * colors.length)];
         } 
+        solution.push(color);
     }
-    console.log(solution)
-}
+};
 randomizeColors();
-
+console.log(solution);
 pickColor = (event) => {
     let color = event.target.id;
     console.log(color);
@@ -52,40 +47,8 @@ revealSolution = () => {
 revealSolution();
 
 /* Event Listeners */
-options.addEventListener("click", pickColor);
 
-
-
-// look at this code from bomb game
-
-// wireState = {
-//     blue: false,
-//     green: false,
-//     red: false,
-//     white: false,
-//     yellow: false,
-//   };
-
-//   // Randomly select which wires need to be cut
-//   for (const color in wireState) {
-//     // console.log(color);
-//     let rand = Math.random(); // gives us a random float between 0.0 and 1.0
-//     if (rand > 0.5) {
-//       wiresToCut.push(color);
-//     }
-//   }
-// }
-
-
-// save a copy
-
-// randomizeColors = () => {
-//     for(i = 0; i < 3; i++) {
-//         let color = colors[Math.floor(Math.random() * colors.length)];
-//         if (!solution.includes(color)){
-//                     solution[i] = color;
-//         } 
-//     }
-//     console.log(solution)
-// }
-// randomizeColors();
+options.forEach(option => {
+    option.addEventListener("click", pickColor);
+});
+// options.addEventListener("click", pickColor);
